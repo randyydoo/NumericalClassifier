cd75d044c8e69c0ee98fb307d59a1e97ae6de98c
Randy Do <randyydoo@csu.fullerton.edu>
2023-05-27 18:42:02 -0700

    prog

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ x_train = np.expand_dims(x_train, axis = -1)
 x_test = np.expand_dims(x_test, axis = -1)
 
 print(x_test.shape)
+
+#build model
+inputs = KL.Input(shape = (28,28,1)) #shape is from x_test and x_train
+#32 input filters each exiting with a weight and feature map, no padding so smaller output(risk of loosing information at edges),
+# 3x3 filter gets applies to 28x28 image to get feature map and results in 3x3 feature map(slding 3x3 onto 28x28 to get clearer image)
+conv_layer = KL.Conv2D(32,(3,3), padding = "valid", activation = tf.nn.relu)(inputs)
+#divide input into regions and returns max value 
+max_pooling = KL.MaxPool2D((2,2), (2,2))(conv_layer)
+#
+flat = KL.Flatten()(max_pooling)
+#
+outputs = KL.Dense(10,activation = tf.nn.softmax)(flat)
+#
+model = KM.Model(inputs, outputs)
+model.summary()
+
