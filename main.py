6ccd6c29b070b72ad07f6499425ed13daa1146e7
Randy Do <randyydoo@csu.fullerton.edu>
2023-05-28 18:24:44 -0700

    added more layers 99% acc

--- main.py
+++ main.py
@@ -23,16 +23,18 @@ x_test = np.expand_dims(x_test, axis = -1)
 #build model
 inputs = KL.Input(shape = (28,28,1)) #shape is from x_test and x_train
 #32 3x3 filters gets applied to 28x28 image with stride 1. 3x3 moves moves over 1 to right and gets largest value 
-#we get 26x26 matrix from [(input_size - filter_size + 2 * padding) / stride] + 1 = [(28 - 3 + 2 * 0) / 1] + 1
-conv_layer = KL.Conv2D(32,(3,3), padding = "valid", activation = tf.nn.relu)(inputs)
+#we get 32 26x26 matrix from [(input_size - filter_size + 2 * padding) / stride] + 1 = [(28 - 3 + 2 * 0) / 1] + 1
+conv_layer = KL.Conv2D(32,(3,3), padding = "same", activation = tf.nn.relu)(inputs)
 
-#divide 3x3 matrix and slide 2x2 matrix over and return a max values onto 2x2 matrix  
-#(13,13,32)
+#divide 3x3 matrix and slide 1x1 matrix over and return a max values onto 2x2 matrix  
 max_pooling = KL.MaxPool2D((2,2), (2,2))(conv_layer)
+conv_layer2 = KL.Conv2D(64,(3,3), padding = "same", activation = tf.nn.relu)(max_pooling)
+
+max_pooling2 = KL.MaxPool2D((2,2), (2,2))(conv_layer2)
+conv_layer3 = KL.Conv2D(128,(3,3), padding = "same", activation = tf.nn.relu)(max_pooling2)
 
 #turn matrix into array and each position has value refereing to intensity of pixel
-#length of 5408 array from 13*13*32
-flat = KL.Flatten()(max_pooling)
+flat = KL.Flatten()(max_pooling2)
 
 #connect layers after the 32 inital nodes to get output
 outputs = KL.Dense(10,activation = tf.nn.softmax)(flat)
@@ -43,7 +45,7 @@ model = KM.Model(inputs, outputs)
 # model.summary()
 # adam optimzer is most common
 model.compile(optimizer = "adam", loss = "sparse_categorical_crossentropy", metrics = ['accuracy'])
-model.fit(x_train,y_train, epochs = 10)
+model.fit(x_train,y_train, epochs = 5)
 test_loss, test_acc = model.evaluate(x_test,y_test)
 print(f"Test Loss: {test_loss} ---- Test Accuracy {test_acc}")
 
