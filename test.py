a804d849d24be91e96f3d48e39810fc21a2b44b6
Randy Do <randyydoo@csu.fullerton.edu>
2023-05-31 18:25:06 -0700

    works

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import cv2 as cv
 import numpy as np
 from tensorflow.keras.models import load_model
 
-model = load_model('MNIST.model')
+model = load_model('MNIST.h5')
 for num in range(1,6):
-    image = cv.imread(f'{num}.png')[:,:,0]
+    image = cv2.imread('images/{num}.png')[:,:,0]
     image = np.invert(np.array([image]))
     prediction = model.predict(image)
     print(f'The number is most likely: {np.argmax(prediction)}')
