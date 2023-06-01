11b31a2c34dc391e25f23a236f75b9f2defcad3b
Randy Do <89406937+randyydoo@users.noreply.github.com>
2023-05-31 18:45:08 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import load_model
 
 model = load_model('MNIST.h5')
 for num in range(1,6):
-    image = f'/Users/randydo/Desktop/github/projects/MINSTclassification/images/{num}.png'
+    image = f'images/{num}.png'
     image = cv.imread(image)[:,:,0]
     image = np.invert(np.array([image]))
     prediction = model.predict(image)
