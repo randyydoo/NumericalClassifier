dcc9f922363ff0bf5171d5addcac592ba9c9be28
Randy Do <randyydoo@csu.fullerton.edu>
2023-05-31 17:41:20 -0700

    added model and pics

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import cv2 as cv
 import tensorflow as tf
 import tensorflow.keras.layers as KL
 import tensorflow.keras.models as KM
-
+import matplotlib.pyplot as plt
 import numpy as np
-
+'''
 #load data
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -21,7 +22,7 @@ x_test = np.expand_dims(x_test, axis = -1)
 
 
 #build model
-inputs = KL.Input(shape = (28,28,1)) #shape is from x_test and x_train
+inputs = KL.Input(shape = (28,28, 1)) #shape is from x_test and x_train
 #32 3x3 filters gets applied to 28x28 image with stride 1. 3x3 moves moves over 1 to right and gets largest value 
 #we get 32 26x26 matrix from [(input_size - filter_size + 2 * padding) / stride] + 1 = [(28 - 3 + 2 * 0) / 1] + 1
 conv_layer = KL.Conv2D(32,(3,3), padding = "same", activation = tf.nn.relu)(inputs)
@@ -45,14 +46,14 @@ model = KM.Model(inputs, outputs)
 # model.summary()
 # adam optimzer is most common
 model.compile(optimizer = "adam", loss = "sparse_categorical_crossentropy", metrics = ['accuracy'])
-model.fit(x_train,y_train, epochs = 5)
+model.fit(x_train,y_train, epochs = 3)
 test_loss, test_acc = model.evaluate(x_test,y_test)
+model.save('MNIST.model')
 print(f"Test Loss: {test_loss} ---- Test Accuracy {test_acc}")
-
-
-
-
-
-
-
-
+'''
+#read images and output predictions
+for num in range(1,6):
+    img = cv.imread(f'{num}.png')[:,:,0]
+    img = np.array([img])
+    plt.imshow(img[0])
+    plt.show()
